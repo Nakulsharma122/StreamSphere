a0f07342cca533dd9e553a6e13ec596bb9950e1a
Nakulsharma122 <nakusharma29@gmail.com>
2025-02-13 14:36:45 +0530

    Header  , login , signup page created

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div >
-   <img  className='w-20' src="https://play-lh.googleusercontent.com/0C0cOYf8rxm7hdx-mrBFi7iTBfUYKzScGJy0s82RKfn2MOh3DJdblO9hmlCqjfD6KeNt=w240-h480-rw" alt="logo"/>
+    <div className='absolute px-5 py-2 z-10' >
+   <img  className='w-20 m-3' src="https://play-lh.googleusercontent.com/0C0cOYf8rxm7hdx-mrBFi7iTBfUYKzScGJy0s82RKfn2MOh3DJdblO9hmlCqjfD6KeNt=w240-h480-rw" alt="logo"/>
+  
    </div>
   )
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,6 +5,17 @@ export const Login = () => {
   return (
     <div>
     <Header/>
+         <div className='absolute'>
+       <img src="https://images.pexels.com/photos/23495578/pexels-photo-23495578/free-photo-of-a-group-of-people-sitting-on-a-couch-watching-tv.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt ="background"/>
+        </div>
+   <form className = "absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-70">
+    <h1 className="font-bold text-3xl px-2 py-3" >Sign In </h1>
+    <input type ='text' placeholder='Email Address ' className='p-4 my-4 w-full bg-gray-700'/>
+    <input type ='password' placeholder='password' className='p-4 my-4 w-full bg-gray-700'/>
+        <button className='p-4 my-4 bg-red-700 w-full rounded-lg'>Sign In</button>
+        <p className='py-3'>New to StreamSphere ? Sign Up Now</p>
+       </form>
+       
     </div>
   )
 }
