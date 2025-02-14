95cc6685b1a451131a4585609f0d8c2ac04e1ef6
Nakulsharma122 <nakusharma29@gmail.com>
2025-02-14 15:15:56 +0530

    Done authetication and deployment

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react'
-import { Header } from './Header'
+import React, { useState,useRef} from 'react'
+import { Header } from './Header';
+import {checkValidData, checkValidData1}  from '../utils/validate';
 
 
 
 export const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () =>{
+
+      const message = isSignInForm ? checkValidData(email.current.value, password.current.value): checkValidData1(name.current.value,email.current.value, password.current.value) ;
+      setErrorMessage(message);
+    
+  }
   const toggleSignInForm = () =>{
     setIsSignInForm (!isSignInForm);
   }
@@ -14,22 +26,25 @@ export const Login = () => {
          <div className='absolute'>
        <img src="https://images.pexels.com/photos/23495578/pexels-photo-23495578/free-photo-of-a-group-of-people-sitting-on-a-couch-watching-tv.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt ="background"/>
         </div>
-   <form className = "absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-70">
-    <h1 className="font-bold text-3xl px-2 py-3" >{isSignInForm ? "Sign  In" :"Sign Up"} </h1>
 
-    {!isSignInForm &&(
-      <input type ='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'/>
-    )}
+     <form  onSubmit={(e)=>{e.preventDefault()}}className = "absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-70">
+        <h1 className="font-bold text-3xl px-2 py-3" >{isSignInForm ? "Sign  In" :"Sign Up"} </h1>
 
-    <input type ='text' placeholder='Email Address ' className='p-4 my-4 w-full bg-gray-700'/>
-    <input type ='password' placeholder='password' className='p-4 my-4 w-full bg-gray-700'/>
-        <button className='p-4 my-4 bg-red-700 w-full rounded-lg'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
-        <p className='py-3 cursor-pointer' onClick={toggleSignInForm}>
-        {isSignInForm ? (
-              <>New to StreamSphere ? <span className="text-red-500">Sign Up Now</span></>
-                ) : (
-          <>Already registered? <span className="text-red-500">Sign In</span></>
-               )}
+      {!isSignInForm &&(
+        <input ref={name} type ='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'/>
+        )}
+    <input type ='text' placeholder='Email Address ' className='p-4 my-4 w-full bg-gray-700' ref={email}/>
+    <input type ='password' placeholder='password' className='p-4 my-4 w-full bg-gray-700' ref= {password}/>
+
+      <p className='text-red-500  font-bold'>{errorMessage}</p>
+
+    <button className='p-4 my-4 bg-red-700 w-full rounded-lg' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+    <p className='py-3 cursor-pointer' onClick={toggleSignInForm}>
+      {isSignInForm ? (
+          <>New to StreamSphere ? <span className="text-red-500 font-bold">Sign Up Now</span></>
+          ) : (
+          <>Already registered? <span className="text-red-500 font-bold">Sign In</span></>
+          )}
         </p>
        </form>
        
